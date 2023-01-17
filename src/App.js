c4f5b45415ffcb4f3305f7970f84318c34a2dc76
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-01-17 15:05:01 -0500

    Counter component done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import Button from './Components/Button';
+import Counter from './Components/Counter';
+import { useState } from 'react';
+
 import './App.css';
 
 
 function App() {
-  const handleClick = () =>{
+  const [clickNumber, setClickNumber] = useState(0); 
 
+  const handleClick = () =>{
+    setClickNumber(clickNumber+1);
   }
 
   const restartCount = () => {
-
+    setClickNumber(0);
   }
-
   return (
     <div className="App">
       <div className='main-container'>
+        <Counter clickNumber={clickNumber}></Counter>
         <Button text="Click" isClick={true} handleClick={handleClick} ></Button>
         <Button text="Restart" isClick={false} handleClick={restartCount} ></Button>
       </div>
